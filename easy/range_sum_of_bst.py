01b02e7f7f06c198c5fed6b2b2fc4eb3d7acea63
James Riall <jamesrariall@gmail.com>
2021-05-18 21:04:44 +0100

    Add improved recursive solution to range sum of bst

--- easy/range_sum_of_bst.py
+++ easy/range_sum_of_bst.py
@@ -24,3 +24,25 @@ class TreeNode:
     self.val = val
     self.left = left
     self.right = right
+
+
+# Improved recursive solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#   def __init__(self, val=0, left=None, right=None):
+#     self.val = val
+#     self.left = left
+#     self.right = right
+class Solution:
+  def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+    if root is None:
+      return 0
+    count = 0
+    if low <= root.val <= high:
+      count += root.val
+    if root.left is not None:
+      count += self.rangeSumBST(root.left, low, high)
+    if root.right is not None:
+      count += self.rangeSumBST(root.right, low, high)
+    return count
