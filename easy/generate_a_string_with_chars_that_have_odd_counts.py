5b01669fa7772fd9f7ec93630542e0e929f4d874
James Riall <jamesrariall@gmail.com>
2021-05-31 20:28:34 +0100

    Add Generate an Odd String...  solution

---
+++ easy/generate_a_string_with_chars_that_have_odd_counts.py
@@ -0,0 +1,23 @@
+# Generate a String With Characters That Have Odd Counts
+
+# Given an integer n, return a string with n characters such that each
+# character in such string occurs an odd number of times.
+
+# The returned string must contain only lowercase English letters. If there are
+# multiples valid strings, return any of them.  
+
+# Example 1:
+
+# Input: n = 4
+# Output: "pppz"
+# Explanation: "pppz" is a valid string since the character 'p' occurs three
+# times and the character 'z' occurs once. Note that there are many other valid
+# strings such as "ohhh" and "love".
+
+class Solution:
+  def generateTheString(self, n: int) -> str:
+    is_odd = n % 2 == 1
+    if is_odd:
+      return 'a' * n
+    else:
+      return 'a' * (n - 1) + 'b'
