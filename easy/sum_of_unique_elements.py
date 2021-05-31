51d4450afcf86fe505229835abce330e6bf2fb76
James Riall <jamesrariall@gmail.com>
2021-05-31 20:34:28 +0100

    Add sum of unique elements solution

---
+++ easy/sum_of_unique_elements.py
@@ -0,0 +1,26 @@
+# Sum of Unique Elements
+
+# You are given an integer array nums. The unique elements of an array are the
+# elements that appear exactly once in the array.
+
+# Return the sum of all the unique elements of nums.
+
+# Example 1:
+
+# Input: nums = [1,2,3,2]
+# Output: 4
+# Explanation: The unique elements are [1,3], and the sum is 4.
+
+class Solution:
+  def sumOfUnique(self, nums: List[int]) -> int:
+    hashtable = {}
+    for num in nums:
+      if num in hashtable:
+        hashtable[num] += 1
+      else:
+        hashtable[num] = 1
+    sum = 0
+    for key, value in hashtable.items():
+      if value == 1:
+        sum += key
+    return sum
