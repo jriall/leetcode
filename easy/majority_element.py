b13e88d410f2e39edce19701b91b42b9341e5a9b
James Riall <jamesrariall@gmail.com>
2021-06-12 19:59:21 +0100

    Improve majority element solution

--- easy/majority_element.py
+++ easy/majority_element.py
@@ -10,6 +10,8 @@
 # Input: nums = [3,2,3]
 # Output: 3
 
+# O(n) time, O(n) space solution
+
 class Solution:
   def majorityElement(self, nums: List[int]) -> int:
     count = {}
@@ -18,11 +20,23 @@ class Solution:
         count[num] += 1
       else:
         count[num] = 1
-    result = None
-    biggest = 0
-    for key, value in count.items():
-      if value > biggest:
-        result = key
-        biggest = value
-    return result
-      
+      if count[num] > len(nums) / 2:
+        return num
+
+# O(n) time, O(1) space solution using majority vote algorithm
+
+class Solution:
+  def majorityElement(self, nums: List[int]) -> int:
+    candidate = None
+    count = 0
+    for num in nums:
+      if candidate is None:
+        candidate = num
+        count = 1
+      elif num == candidate:
+        count += 1
+      elif num != candidate:
+        count -= 1
+        if count == 0:
+          candidate = None
+    return candidate
