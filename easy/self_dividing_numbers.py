89f607bb83192ee645a4967ff43bbc16cc5b15db
James Riall <jamesrariall@gmail.com>
2021-06-17 09:26:42 +0100

    Add Self Dividing Numbers solution

---
+++ easy/self_dividing_numbers.py
@@ -0,0 +1,36 @@
+# Self Dividing Numbers
+
+# A self-dividing number is a number that is divisible by every digit it
+# contains.
+
+# For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0,
+# and 128 % 8 == 0.
+# A self-dividing number is not allowed to contain the digit zero.
+
+# Given two integers left and right, return a list of all the self-dividing
+# numbers in the range [left, right].
+
+# Example 1:
+
+# Input: left = 1, right = 22
+# Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+# Example 2:
+
+# Input: left = 47, right = 85
+# Output: [48,55,66,77]
+
+class Solution:
+  def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+    result = []
+    for num in range(left, right + 1):
+      divisor = num
+      to_include = True
+      while divisor > 0:
+        new_num = divisor % 10
+        if new_num == 0 or num % new_num != 0:
+          to_include = False
+          break
+        divisor = divisor // 10
+      if to_include:
+        result.append(num)
+    return result
