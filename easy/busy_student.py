690b5da5a331da5e80086cdcc6a2e3840f8eb552
James Riall <jamesrariall@gmail.com>
2021-06-23 15:47:51 +0100

    Add busy student solution

---
+++ easy/busy_student.py
@@ -0,0 +1,32 @@
+# Number of Students Doing Homework at a Given Time
+
+# Given two integer arrays startTime and endTime and given an integer queryTime.
+
+# The ith student started doing their homework at the time startTime[i] and
+# finished it at time endTime[i].
+
+# Return the number of students doing their homework at time queryTime. More
+# formally, return the number of students where queryTime lays in the interval
+# [startTime[i], endTime[i]] inclusive.
+
+# Example 1:
+
+# Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+# Output: 1
+# Explanation: We have 3 students where:
+# The first student started doing homework at time 1 and finished at time 3 and
+# wasn't doing anything at time 4.
+# The second student started doing homework at time 2 and finished at time 2 and
+# also wasn't doing anything at time 4.
+# The third student started doing homework at time 3 and finished at time 7 and
+# was the only student doing homework at time 4.
+
+class Solution:
+  def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+    result = 0
+    index = 0
+    while index < len(startTime):
+      if queryTime >= startTime[index] and queryTime <= endTime[index]:
+        result += 1
+      index += 1
+    return result
