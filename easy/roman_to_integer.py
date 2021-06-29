cab3a0b547ebdea3bace0f4cdbbb4714fdf4da14
James Riall <jamesrariall@gmail.com>
2021-06-29 23:11:36 +0100

    Improve roman to integer solution

--- easy/roman_to_integer.py
+++ easy/roman_to_integer.py
@@ -1,34 +1,22 @@
 # Roman to integer
 
 VALUES = {
-  'I': 1,
-  'V': 5,
-  'X': 10,
-  'L': 50,
-  'C': 100,
-  'D': 500,
-  'M': 1000,
-}
-
-PREFIXED_VALUES = {
-  'IV': 4,
-  'IX': 9, 
-  'XL': 40,
-  'XC': 90,
-  'CD': 400,
-  'CM': 900,
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
 }
 
 class Solution:
   def romanToInt(self, s: str) -> int:
-    total = 0
-    index = len(s) - 1
+    total = VALUES[s[-1]]
+    index = len(s) - 2
     while index >= 0:
-      print(index, s[index])
-      if index > 0 and s[index-1:index+1] in PREFIXED_VALUES:
-        total += PREFIXED_VALUES[s[index-1:index+1]]
-        index -= 2
-      elif s[index] in VALUES:
-        total += VALUES[s[index]]
-        index -= 1
+      value = VALUES[s[index]]
+      previous_value = VALUES[s[index + 1]]
+      total += value if value >= previous_value else -value
+      index -= 1
     return total
