d811968a821584fa3255af95a18f943fcad607ca
James Riall <jamesrariall@gmail.com>
2021-07-07 13:01:38 +0100

    Add Dot Product of Two Sparse Vectors solution

---
+++ medium/dot_product_of_two_sparse_vectors.py
@@ -0,0 +1,41 @@
+# Dot Product of Two Sparse Vectors
+
+# Given two sparse vectors, compute their dot product.
+
+# Implement class SparseVector:
+
+# SparseVector(nums) Initializes the object with the vector nums
+# dotProduct(vec) Compute the dot product between the instance of SparseVector
+# and vec
+# A sparse vector is a vector that has mostly zero values, you should store the
+# sparse vector efficiently and compute the dot product between two
+# SparseVector.
+
+# Follow up: What if only one of the vectors is sparse?
+
+# Example 1:
+
+# Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+# Output: 8
+# Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+# v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+
+class SparseVector:
+  def __init__(self, nums: List[int]):
+    self.values = {}
+    for index, num in enumerate(nums):
+      if num != 0:
+        self.values[index] = num
+
+  # Return the dotProduct of two sparse vectors
+  def dotProduct(self, vec: 'SparseVector') -> int:
+    result = 0
+    for key, value in vec.values.items():
+      if key in self.values:
+        result += (value * self.values[key])
+    return result
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
